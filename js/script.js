ffa9a8d6a32c5e37bb22c8215fc15a5e8638907e
Shomu1999 <soumyadeepdas.259@gmail.com>
2025-07-11 15:31:14 +0530

    initial commit: foundation structure

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let btn = document.getElementById("createNew");
+btn.addEventListener("click", function() {
+    window.location.href = "create/create.html";
+});
+
+// function save to localstorage
+// function all pixels
+// function archive
+// function bin
+// function search
